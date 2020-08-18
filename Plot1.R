0d1637372647aca0bcc32d2594cc55a60b9b4e0e
javi00 <jvevallejos@gmail.com>
2020-08-17 20:02:56 -0400

    second

--- Plot1.R
+++ Plot1.R
@@ -1,3 +1,14 @@
+library(datasets)
+
+# first unzip the file and  read like a table
+data <- read.table(unz("exdata_data_household_power_consumption.zip", "household_power_consumption.txt"), sep = ";", header = TRUE, colClasses = "character")
+
+# open a window device 480 x 480
 
 
-data <- read.table(file = "household_power_consumption.txt", sep = ";", header = TRUE, stringsAsFactors = FALSE)
+# remove the NA or ? data
+dataf <-  subset(data, Global_active_power != "?")
+summary(as.numeric(dataf$Global_active_power))
+dataf$Date <- as.Date(dataf$Date, "%d/%m/%Y")
+dataf$Global_active_power <- as.numeric(dataf$Global_active_power)
+
